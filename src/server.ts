c7a2edf2ccd06d11f05aa09cd8c563855f1b80d6
peterdinis <peterdinis0@pobox.sk>
2021-07-15 20:56:42 +0200

    Fixing problem in server

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,29 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import express, {Request, Response, Application} from "express";
+import express, { Request, Response, Application } from "express";
 import morgan from "morgan";
-import chalk from "chalk";
 import helmet from "helmet";
 import dbConfig from "./config/database";
 
 const app: Application = express();
-const PORT = 4001;
 
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("TEST");
+app.get("/", (req: Request, res: Response) => {
+  res.send("TEST");
 })
+
+// json parsing
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 createConnection(dbConfig)
   .then((_connection) => {
-    app.listen(PORT, () => {
-      console.log("Server is running on port", PORT);
+    app.listen(4200, () => {
+      console.log("Server is running on port", 4200);
     });
   })
   .catch((err) => {
     console.log("Unable to connect to db", err);
-    process.exit(1);
   });
-
-app.listen(PORT, () => {
-    chalk.green.inverse("Applikácia beží na porte 4000");
-})
