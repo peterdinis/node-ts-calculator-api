ee00e3fd0dc2a164be9dfbf3900406ab33c151bb
peterdinis <peterdinis0@pobox.sk>
2021-07-15 21:46:46 +0200

    Services update

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,6 +1,6 @@
 import {getConnection} from "typeorm";
+import { ProductEntity } from "../entities/productEntity";
 import {ProductRepository} from "../repository/ProductRepository"
-
 
 const productRepository = getConnection("products").getCustomRepository(ProductRepository);
 
@@ -9,15 +9,18 @@ export const getAllService = async () => {
     return products;
 }
 
-export const createService = () => {
-    return "WORK";
+export const createService = async (product: ProductEntity) => {
+    const newProduct = await productRepository.save(product);
+    return newProduct;
 }
 
-export const updateService = () => {
-    return "WORK";
+export const updateService = async (id: number, product: ProductEntity) => {
+    const updatedProduct = await productRepository.update(id, product);
+    return updatedProduct;
 }
 
-export const deleteService = () => {
-    return "WORK";
+export const deleteService = async (id: number) => {
+    const deletedProduct = await productRepository.delete(id);
+    return deletedProduct;
 }
 
