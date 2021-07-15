79b36f9478c5749fd1c66ad7c8cfc494746e4f6e
peterdinis <peterdinis0@pobox.sk>
2021-07-15 15:30:44 +0200

    Start to editing pg

---
+++ src/config/database.ts
@@ -0,0 +1,14 @@
+import { ConnectionOptions } from "typeorm";
+
+const config: ConnectionOptions = {
+  type: "postgres",
+  host: process.env.POSTGRES_HOST || "localhost",
+  port: Number(process.env.POSTGRES_PORT) || 5432,
+  username: process.env.POSTGRES_USER || "postgres",
+  password: process.env.POSTGRES_PASSWORD || "PETERdinis1234",
+  database: process.env.POSTGRES_DB || "calculatordb",
+  entities: [],
+  synchronize: true,
+};
+
+export default config;
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log("WORK");
+import express, {Request, Response} from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("TEST");
+})
+
+app.listen(process.env.PORT || 4000, () => {console.log("server started");
+})
