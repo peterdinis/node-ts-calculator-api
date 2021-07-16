54fc0b73012c125bb2ac59be291536042db8fe13
peterdinis <peterdinis0@pobox.sk>
2021-07-16 12:38:38 +0200

    Repository update

---
+++ src/repository/ProductRepository.ts
@@ -0,0 +1,19 @@
+import {Product} from "../models";
+import {getRepository} from "typeorm"
+import {IProduct} from "../interfaces/IProduct";
+
+
+export const getAllProducts = async(): Promise<Array<IProduct>> => {
+    const productRepository = getRepository(Product);
+    return productRepository.find();
+}
+
+export const createProduct = async (payload: IProduct): Promise<Product> => {
+    const productRepository = getRepository(Product);
+
+    const product = new Product();
+    return productRepository.save({
+        ...product,
+        ...payload
+    })
+}
